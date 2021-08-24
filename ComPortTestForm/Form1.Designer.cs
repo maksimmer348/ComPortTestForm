27a88b6bb77635edb5bc975d103231b0625ff1c6
maksimmer348 <maksimmer348@gmail.com>
2021-08-24 09:00:44 +0300

    использование UseDataReceived

---
+++ ComPortTestForm/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace ComPortTestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSetValue = new System.Windows.Forms.Button();
+            this.textBoxSetV = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSetA = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxGetA = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxGetV = new System.Windows.Forms.TextBox();
+            this.buttonOutput = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonSetValue
+            // 
+            this.buttonSetValue.Location = new System.Drawing.Point(12, 91);
+            this.buttonSetValue.Name = "buttonSetValue";
+            this.buttonSetValue.Size = new System.Drawing.Size(51, 23);
+            this.buttonSetValue.TabIndex = 0;
+            this.buttonSetValue.Text = "установка";
+            this.buttonSetValue.UseVisualStyleBackColor = true;
+            this.buttonSetValue.Click += new System.EventHandler(this.buttonSetValue_Click);
+            // 
+            // textBoxSetV
+            // 
+            this.textBoxSetV.Location = new System.Drawing.Point(12, 26);
+            this.textBoxSetV.Name = "textBoxSetV";
+            this.textBoxSetV.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSetV.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "установить V";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "установить A";
+            // 
+            // textBoxSetA
+            // 
+            this.textBoxSetA.Location = new System.Drawing.Point(12, 65);
+            this.textBoxSetA.Name = "textBoxSetA";
+            this.textBoxSetA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSetA.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "считать A";
+            // 
+            // textBoxGetA
+            // 
+            this.textBoxGetA.Location = new System.Drawing.Point(118, 65);
+            this.textBoxGetA.Name = "textBoxGetA";
+            this.textBoxGetA.ReadOnly = true;
+            this.textBoxGetA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGetA.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(118, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "считать V";
+            // 
+            // textBoxGetV
+            // 
+            this.textBoxGetV.Location = new System.Drawing.Point(118, 26);
+            this.textBoxGetV.Name = "textBoxGetV";
+            this.textBoxGetV.ReadOnly = true;
+            this.textBoxGetV.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGetV.TabIndex = 6;
+            // 
+            // buttonOutput
+            // 
+            this.buttonOutput.Location = new System.Drawing.Point(165, 91);
+            this.buttonOutput.Name = "buttonOutput";
+            this.buttonOutput.Size = new System.Drawing.Size(53, 23);
+            this.buttonOutput.TabIndex = 5;
+            this.buttonOutput.Text = "выход";
+            this.buttonOutput.UseVisualStyleBackColor = true;
+            this.buttonOutput.Click += new System.EventHandler(this.buttonOutput_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(87, 91);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(53, 23);
+            this.buttonRefresh.TabIndex = 10;
+            this.buttonRefresh.Text = "обновление";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 126);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxGetA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxGetV);
+            this.Controls.Add(this.buttonOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSetA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSetV);
+            this.Controls.Add(this.buttonSetValue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSetValue;
+        private System.Windows.Forms.TextBox textBoxSetV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSetA;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxGetA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxGetV;
+        private System.Windows.Forms.Button buttonOutput;
+        private System.Windows.Forms.Button buttonRefresh;
+    }
+}
+
---
+++ ComPortTestForm/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GodSharp.SerialPort;
+using static ComPortTestForm.CommandsSupplyPSH;
+
+namespace ComPortTestForm
+{
+    public partial class Form1 : Form
+    {
+        MySerialPort Serial = new MySerialPort(4, 9600, 0);
+        private Task RunTask;//рабочий поток
+        protected ManualResetEvent Suspense = new ManualResetEvent(true);//для приостановки петли измерений значений
+        public Form1()
+        {
+            InitializeComponent();
+            Serial.DataReceived += RefreshValues;
+            Serial.Write(OUTPUT_OFF);
+        }
+
+        private void buttonSetValue_Click(object sender, EventArgs e)
+        {
+            RunTask = Task.Run(() => SetValues());
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            RunTask = Task.Run(() => WorkRepeat());
+        }
+
+        private void WorkRepeat()
+        {
+            while (true)
+            {
+                Serial.Write(RETURN_SET_CURRENT);
+                Thread.Sleep(50);
+                Serial.Read("ток");
+                Thread.Sleep(50);
+                Suspense.WaitOne();
+
+                Serial.Write(RETURN_SET_VOLTAGE);
+                Thread.Sleep(50);
+                Serial.Read("напряжение");
+                Thread.Sleep(50);
+                Suspense.WaitOne();
+
+                Serial.Write(RETURN_OUTPUT);
+                Thread.Sleep(50);
+                Serial.Read("выход");
+                Thread.Sleep(50);
+                Suspense.WaitOne();
+
+                Serial.Write("* IDN?");
+                Thread.Sleep(50);
+                Serial.Read("ответ");
+                Thread.Sleep(50);
+                Suspense.WaitOne();
+            }
+        }
+
+        private void buttonOutput_Click(object sender, EventArgs e)
+        {
+            RunTask = Task.Run(() => OutputSwitch());
+        }
+
+        void OutputSwitch()
+        {
+            Suspense.Reset();// приостановка петли измерений значений, для отправки команды в прибор
+            Serial.Write(RETURN_OUTPUT);
+            Thread.Sleep(50);
+            Serial.Read();
+            Thread.Sleep(50);
+            if (Response =="1")
+            {
+                Serial.Write(OUTPUT_OFF);
+            }
+            else if (Response == "0")
+            {
+                Serial.Write(OUTPUT_ON);
+            }
+            Suspense.Set();//продолить петлю измерений значений
+        }
+
+        private bool OutputEnable = false;
+
+        void RefreshValues(string value)
+        {
+            Debug.Write(value);
+            Response = value;
+        }
+
+        private string Response;
+
+        void SetValues()
+        {
+            Serial.Write(SET_VOLTAGE + " " + textBoxSetV.Text);
+            Serial.Write(SET_CURRENT + " " + textBoxSetA.Text);
+        }
+    }
+
+    public class MySerialPort
+    {
+        private readonly int Number;
+        private readonly int BaudRate;
+        private readonly int Parity;
+        private GodSerialPort Serial;
+
+        public Action<string> DataReceived;
+        public event Action<Exception> ReceiveErrorMessage;//вывод исключений
+
+        public bool flag;
+
+        public MySerialPort(int number, int baudRate, int parity)
+        {
+            Number = number;
+            BaudRate = baudRate;
+            Parity = parity;
+        }
+
+
+        public void Open()
+        {
+            if (Serial == null || !Serial.IsOpen)
+            {
+                try
+                {
+                    Serial = new GodSerialPort($"COM{Number}", BaudRate, Parity);
+                    Serial.Open();
+                    flag = true;
+                }
+                catch (Exception exception)
+                {
+                    throw exception;
+                }
+            }
+        }
+
+        public void Write(string message)
+        {
+            Open();
+            const string END_OF_LINE = "\r\n";
+            try
+            {
+                var dataBytes = Encoding.UTF8.GetBytes(message + END_OF_LINE);
+                Serial.Write(dataBytes);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        /// <summary>
+        /// чтение из компорта
+        /// </summary>
+        /// <param name="cmd">передача команды в ответе для идентификации</param>
+        /// <param name="cmdRequsetCmd"></param>
+        /// <param name="command"></param>
+        public void Read(string command = null)
+        {
+            try
+            {
+                Serial.UseDataReceived(flag, (port, bytes) =>
+                {
+                    string buffer = Encoding.UTF8.GetString(bytes);
+                    DataReceived?.Invoke(command + " " + buffer);
+                });
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        public void Close()
+        {
+            if (Serial != null && Serial.IsOpen)
+            {
+                try
+                {
+                    flag = false;
+                    Serial.Close();
+
+                }
+                catch (Exception exception)
+                {
+                    ReceiveErrorMessage?.Invoke(exception);
+                }
+            }
+        }
+
+
+        static string RemoveUnnecessary(string message)//удаление мусора для строки ответа
+        {
+            var unnecessary = new[] { '?', '\n', '\r' };
+            return String.Join("", message.Where((ch) => !unnecessary.Contains(ch)));
+        }
+
+    }
+
+    public static class CommandsSupplyPSH
+    {
+        public const string SET_VOLTAGE = ":chan1:volt ";
+        public const string RETURN_VOLTAGE = ":chan1:meas:volt ?";
+        public const string RETURN_SET_VOLTAGE = ":chan1:volt ?";
+
+        public const string SET_CURRENT = ":chan1: curr ";
+        public const string RETURN_CURRENT = ":chan1:meas:curr ?";
+        public const string RETURN_SET_CURRENT = ":chan1:curr ?";
+
+        public const string OUTPUT_ON = ":outp:stat 1";
+        public const string OUTPUT_OFF = ":outp:stat 0";
+        public const string RETURN_OUTPUT = ":outp:stat?";
+    }
+}
